6e179b81d006b04c5f300839b675ec538bc56ffd
Jonathan Gan <jonganej@gmail.com>
2018-10-31 11:44:25 +0100

    Added commander for nicer command line interface

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const program = require('commander');
 
-if (process.argv.length <= 2)
-  throw new Error('Path to Swagger API 2.0 specs file required.');
-
-const specsFile = process.argv[2];
-fs.readFile(specsFile, (err, data) => {
-  const jsonFile = JSON.parse(data);
-  Object.getOwnPropertyNames(jsonFile.paths).forEach(pathName => {
-    const operations = jsonFile.paths[pathName];
-    Object.getOwnPropertyNames(operations).forEach(methodName => {
-      const operation = operations[methodName];
-      console.log(operation);
-      if (Object.prototype.hasOwnProperty.call(operation, 'parameters')) {
-        operation.parameters.forEach(param => {
-          if (param.in === 'body') {
-            param.name = `${operation.operationId}-body`;
+program
+  .version('1.0.0')
+  .description(
+    'Converts a general Swagger 2.0 API Specification to an Azure-compliant API specification.'
+  )
+  .command(
+    '<file>',
+    'Convert given file to Azure-compliant Swagger specification.'
+  )
+  .action(file => {
+    fs.readFile(file, (err, data) => {
+      const jsonFile = JSON.parse(data);
+      Object.getOwnPropertyNames(jsonFile.paths).forEach(pathName => {
+        const operations = jsonFile.paths[pathName];
+        Object.getOwnPropertyNames(operations).forEach(methodName => {
+          const operation = operations[methodName];
+          if (Object.prototype.hasOwnProperty.call(operation, 'parameters')) {
+            operation.parameters.forEach(param => {
+              if (param.in === 'body') {
+                param.name = `${operation.operationId}-body`;
+              }
+            });
           }
         });
-      }
+      });
+      fs.writeFile(file, JSON.stringify(jsonFile, null, 2), 'utf-8', err => {
+        if (err) {
+          throw new Error(err);
+        }
+        console.log(
+          'The API specification file was successfully converted and saved!'
+        );
+      });
     });
   });
-  fs.writeFile(specsFile, JSON.stringify(jsonFile, null, 2), 'utf-8', err => {
-    if (err) {
-      throw new Error(err);
-    }
-    console.log(
-      'The API specification file was successfully converted and saved!'
-    );
-  });
-});
+
+program.parse(process.argv);
